86a7d162d8ad76c2dd1b78af3183f830cf348fad
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-02-10 18:42:44 +0900

    pod init

---
+++ 15min_mokumoku/Pods/Target Support Files/Pods-15min_mokumoku-15min_mokumokuUITests/Pods-15min_mokumoku-15min_mokumokuUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_15min_mokumoku_15min_mokumokuUITests : NSObject
+@end
+@implementation PodsDummy_Pods_15min_mokumoku_15min_mokumokuUITests
+@end
